22ae1e2db4bbca67ef0c5ba3088f334ad739e5d4
katherinesanders <katherinespro@gmail.com>
2021-08-31 21:19:33 -0600

    attempted call buttons and added hulu shows

--- Eupheme/HotlinesViewController.swift
+++ Eupheme/HotlinesViewController.swift
@@ -6,15 +6,90 @@
 //
 
 import UIKit
+import MessageUI
 
-class HotlinesViewController: UIViewController {
+class HotlinesViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
+
+    @IBOutlet weak var sphCallRound: UIButton!
+    @IBOutlet weak var namiTextRound: UIButton!
+    @IBOutlet weak var namiCallRound: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sphCallRound.layer.cornerRadius = 10
+        namiTextRound.layer.cornerRadius = 10
+        namiCallRound.layer.cornerRadius = 10
 
         // Do any additional setup after loading the view.
     }
     
+
+    @IBAction func sphCallButton(_ sender: UIButton) {
+        UIApplication.shared.open(NSURL(string: "tel://18002738255")! as URL)
+    }
+    
+    
+    @IBAction func namiTextButton(_ sender: UIButton) {
+        if (MFMessageComposeViewController.canSendText()) {
+                    let controller = MFMessageComposeViewController()
+                    controller.body = "NAMI"
+                    controller.recipients = ["741741"]
+                    controller.messageComposeDelegate = self
+                    self.present(controller, animated: true, completion: nil)
+                }
+    }
+    
+    
+    @IBAction func namiCallButton(_ sender: UIButton) {
+        if let url = URL(string: "tel://\(18002738255)") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController,
+            didFinishWith result: MessageComposeResult) {
+            switch (result) {
+                case .cancelled:
+                    print("Message was cancelled")
+                    dismiss(animated: true, completion: nil)
+                case .failed:
+                    print("Message failed")
+                    dismiss(animated: true, completion: nil)
+                case .sent:
+                    print("Message was sent")
+                    dismiss(animated: true, completion: nil)
+                default:
+                    break
+            }
+        }
+    
+    /*
+     
+     Message code:
+        
+     if (MFMessageComposeViewController.canSendText()) {
+                 let controller = MFMessageComposeViewController()
+                 controller.body = "NAMI"
+                 controller.recipients = ["741741"]
+                 controller.messageComposeDelegate = self
+                 self.present(controller, animated: true, completion: nil)
+             }
+     
+     Call Code:
+     
+     if let url = URL(string: "tel://\(18002738255)") {
+         UIApplication.shared.open(url)
+     }
+     
+     */
+    
+    
+    //1-800-950-6264
 
     /*
     // MARK: - Navigation
--- Eupheme/HuluShowsViewController.swift
+++ Eupheme/HuluShowsViewController.swift
@@ -9,9 +9,43 @@ import UIKit
 
 class HuluShowsViewController: UIViewController {
 
+    
+    @IBOutlet weak var manifestImage: UIImageView!
+    @IBOutlet weak var manifestLearn: UIButton!
+    
+    @IBOutlet weak var screamQueensImage: UIImageView!
+    @IBOutlet weak var screamQueensLearn: UIButton!
+    
+    @IBOutlet weak var aloneImage: UIImageView!
+    @IBOutlet weak var aloneLearn: UIButton!
+    
+    @IBOutlet weak var shamelessImage: UIImageView!
+    @IBOutlet weak var shamelessLearn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manifestImage.layer.cornerRadius = 20
+        self.manifestImage.layer.masksToBounds = true;
+        manifestLearn.layer.cornerRadius = 10
+        self.manifestLearn.layer.masksToBounds = true;
 
+        screamQueensImage.layer.cornerRadius = 20
+        self.screamQueensImage.layer.masksToBounds = true;
+        screamQueensLearn.layer.cornerRadius = 10
+        self.screamQueensLearn.layer.masksToBounds = true;
+        
+        aloneImage.layer.cornerRadius = 20
+        self.aloneImage.layer.masksToBounds = true;
+        aloneLearn.layer.cornerRadius = 10
+        self.aloneLearn.layer.masksToBounds = true;
+        
+        shamelessImage.layer.cornerRadius = 20
+        self.shamelessLearn.layer.masksToBounds = true;
+        manifestLearn.layer.cornerRadius = 10
+        self.shamelessLearn.layer.masksToBounds = true;
+        
+        
         // Do any additional setup after loading the view.
     }
     
